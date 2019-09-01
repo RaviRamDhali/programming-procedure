d1aa7e20832a5b8927325532e643900d145fce90
Ravi Ram <ravi@dhali.com>
2019-08-31 17:44:30 -0700

    Update Component.js

--- Snippets/VueJs/Component.js
+++ Snippets/VueJs/Component.js
@@ -10,6 +10,11 @@ html
 
 app.js
 
+
+
+// **** Define Vue.component() before new vue().
+// *** Vue.component at the top of JS page
+
 // Define a new component
 Vue.component('users-list', {
     template: '<h1>{{item.name}}</h1>',
