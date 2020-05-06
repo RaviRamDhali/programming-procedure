d952328b27ec982d5245ee5b36a62164e62ff3b4
Ravi Ram <ravi@dhali.com>
2020-05-06 14:49:23 -0700

    Update Last Action by User in Log.sql

--- Snippets/Sql/Last Action by User in Log.sql
+++ Snippets/Sql/Last Action by User in Log.sql
@@ -1,3 +1,8 @@
+SELECT * FROM customer c
+OUTER APPLY(SELECT top 1 * FROM purchase pi 
+WHERE pi.customer_id = c.Id order by pi.Id desc) AS [LastPurchasePrice]
+
+
 select t.applicantId, a.last_name,a.first_name, t.created, t.description, t.usr_id --, t.actionId, t.progressId
 from hr_app_tracking t
 inner join (
