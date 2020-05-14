73778211e65b0262fabc87e55c00b2257316c56d
Ravi Ram <ravi@dhali.com>
2020-05-14 10:22:04 -0700

    Create sample.js

---
+++ Snippets/JavaScript/jQuery-File-Upload/sample.js
@@ -0,0 +1,10 @@
+$('#fileupload').fileupload({
+    loadImageMaxFileSize: 20000000, // 20MB
+    previewMinHeight: 164,
+    previewMaxHeight: 164,
+    previewMinWidth: null,
+    previewMaxWidth: null,
+    previewThumbnail: false,
+    previewCanvas: false,
+    previewOrientation: false
+});
