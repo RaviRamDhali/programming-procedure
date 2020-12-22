df7f4ae225f557778c35559c1acfda0eadafdd25
Ravi Ram <ravi@dhali.com>
2020-12-22 04:26:39 -0800

    Update basic.sql

--- Snippets/Sql/JsonSQL/basic.sql
+++ Snippets/Sql/JsonSQL/basic.sql
@@ -1,35 +1,35 @@
 --------------------------------------------
 ----- JSON QUERY ---------------------------
 --------------------------------------------
--- caseid, caseactivityid, casepersonid, casedocumentsid
+-- id, activityid, personid, documentsid
 
 set statistics time on 
 
-DECLARE @caseId NVARCHAR(100)
+DECLARE @Id NVARCHAR(100)
 DECLARE @type NVARCHAR(100)
 DECLARE @typeId NVARCHAR(100)
 
-SET @caseId = '8251'
-SET @type = 'caseactivityid'
+SET @Id = '8251'
+SET @type = 'activityid'
 SET @typeId = '42121'
 
 SELECT 
 ClientId
-,JSON_QUERY(MetaData, '$.CaseId') AS CaseIds
-,JSON_QUERY(MetaData, '$.CaseActivityId') AS CaseActivityId
-,JSON_QUERY(MetaData, '$.CasePersonId') AS CasePersonId
-,JSON_QUERY(MetaData, '$.CaseDocumentsId') AS CaseDocumentsId
+,JSON_QUERY(MetaData, '$.Id') AS Ids
+,JSON_QUERY(MetaData, '$.ActivityId') AS ActivityId
+,JSON_QUERY(MetaData, '$.PersonId') AS PersonId
+,JSON_QUERY(MetaData, '$.DocumentsId') AS DocumentsId
 ,*
 FROM dbo.Tags
 WHERE 1 = 1 
 AND  ClientId = 6
-AND '8251' IN (SELECT value FROM OPENJSON(MetaData,'$.CaseId'))
+AND '8251' IN (SELECT value FROM OPENJSON(MetaData,'$.Id'))
 AND 
-    (@type = LOWER('caseactivityid') AND @typeId IN (SELECT value FROM OPENJSON(MetaData,'$.CaseActivityId')))
+    (@type = LOWER('activityid') AND @typeId IN (SELECT value FROM OPENJSON(MetaData,'$.ActivityId')))
     OR
-    (@type = LOWER('casepersonid') AND @typeId IN (SELECT value FROM OPENJSON(MetaData,'$.CasePersonId')))
+    (@type = LOWER('personid') AND @typeId IN (SELECT value FROM OPENJSON(MetaData,'$.PersonId')))
     OR
-    (@type = LOWER('casedocumentsid') AND @typeId IN (SELECT value FROM OPENJSON(MetaData,'$.CaseDocumentsId')))
+    (@type = LOWER('documentsid') AND @typeId IN (SELECT value FROM OPENJSON(MetaData,'$.DocumentsId')))
 	
 
 set statistics time off
