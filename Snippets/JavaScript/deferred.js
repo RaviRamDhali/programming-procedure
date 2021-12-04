ce0c74826083c94a9ac8287433f45e0bc1c8e7f8
Ravi Ram <ravi@dhali.com>
2021-12-04 07:47:27 -0800

    Create deferred.js

---
+++ Snippets/JavaScript/deferred.js
@@ -0,0 +1,40 @@
+// Await value
+SignUp(customer, password, userIp).then(
+    function (value) {
+        console.log('%c SignUp THEN ', 'background: black; color: yellow', value);
+    },
+);
+
+
+// Await axios POST
+function SignUp(customer, password, userIp) {
+
+    var result = new $.Deferred();
+
+    PostSignUp(customer, password, userIp)
+        .then(data => {
+
+            setTimeout(() => {
+                result.resolve(data);
+            }, 2000);
+
+
+        })
+        .catch(err => console.log(err))
+
+    return result.promise();
+}
+
+
+// Axios POST with return promise
+function PostSignUp(customer, password, userIp) {
+
+    console.log('%c PostSignUp called', 'background: black; color: yellow');
+
+    return axios.post('/api/customer/signUp', {
+        data: customer,
+        password: password,
+        userIp: userIp
+    }).then(response => response.data.d);
+
+}
