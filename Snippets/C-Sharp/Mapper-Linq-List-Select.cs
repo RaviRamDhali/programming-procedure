f5e7b835cf3493282f9abf9decac1060c2f9618e
Ravi Ram <ravi@dhali.com>
2022-02-19 13:02:52 -0800

    Update Mapper-Linq-List-Select.cs

--- Snippets/C-Sharp/Mapper-Linq-List-Select.cs
+++ Snippets/C-Sharp/Mapper-Linq-List-Select.cs
@@ -1,6 +1,21 @@
 public List<BLL.ViewModel.Customer> CustomerFromDb(List<DAL.ModelDb.Customer> customers)
 {
+    // #1 Group by linq expression
     return customer.Select(CustomerFromDb).ToList();
+    
+    // #2 Foreach into Linq expression
+    return customerPSTAdmins.Select(data => CustomerPSTAdminFromDb(data)).ToList();
+    
+    // #3 Foreach
+    var list = new List<BLL.ViewModel.CustomerPSTAdmin>();
+
+            foreach (var data in customerPSTAdmins)
+            {
+                list.Add(CustomerPSTAdminFromDb(data));
+            }
+
+    return list;
+    
 }
 
 public BLL.ViewModel.Customer CustomerFromDb(DAL.ModelDb.Customer data)
