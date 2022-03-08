d5eee75c42ca2e5d732587543be9edabdf06f0ba
Ravi Ram <ravi@dhali.com>
2022-03-07 16:34:54 -0800

    Update helpers.js

--- Snippets/VueJs/helpers.js
+++ Snippets/VueJs/helpers.js
@@ -1,4 +1,6 @@
-// Moment JS
+// Moment JS HTML5
 dateYearOnlyr: moment().format('YY') + '-',
 dateHtml5: moment().format(moment.HTML5_FMT.DATE),
 
+DateBegin: moment().add(1, 'D').format(moment.HTML5_FMT.DATE),
+DateEnd: moment().add(1, 'M').format(moment.HTML5_FMT.DATE),
