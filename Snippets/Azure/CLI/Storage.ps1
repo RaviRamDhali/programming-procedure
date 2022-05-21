de3db5c1b2017942634bc3f95dbf1f1565d5c7e3
Ravi Ram <ravi@dhali.com>
2022-05-21 15:33:28 -0700

    Create Storage.ps1

---
+++ Snippets/Azure/CLI/Storage.ps1
@@ -0,0 +1,32 @@
+# Data protection
+# Enable soft delete for blobs
+# Enable soft delete for containers
+
+# az
+# az login
+# az storage account list
+# az account set --subscription xxxxf11-xxxx-xxxx-xxxxx-xxxxxxxxxxxxxxxx
+# az storage account list
+# az storage account keys list -n prodbackup
+# az storage container list
+
+$resourceGroup="Development"
+$storageAccount="prodbackup"
+
+$connectionString=az storage account show-connection-string -n $storageAccount -g $resourceGroup --query connectionString -o tsv
+$env:AZURE_STORAGE_CONNECTION_STRING = $connectionString
+# az storage container create -n "private-test"
+
+#Upload a single file
+#az storage fs file upload --source "C:\_temp\sample.txt" -p sample.txt -f private-test
+
+#Upload a folder
+#az storage fs directory upload -f private-test -s "C:\_temp\dhali" --recursive
+
+#Delete a folder
+az storage fs directory delete -n xxxxxx -f private-test
+
+az storage fs delete -n private-test
+
+
+#az storage fs directory upload -f intranet -s "D:\_temp\" --recursive
