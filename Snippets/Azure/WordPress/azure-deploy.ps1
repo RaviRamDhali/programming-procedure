e19c3a147964ba85689d2e8807cb0512ce0f2c18
Ravi Ram <ravi@dhali.com>
2022-08-07 07:53:49 -0700

    Update azure-deploy.ps1

--- Snippets/Azure/WordPress/azure-deploy.ps1
+++ Snippets/Azure/WordPress/azure-deploy.ps1
@@ -1,6 +1,24 @@
+Clear-Host
+Write-Host "Enter the name of the WordPress website and MySQL database to be created" -ForeGround Yellow
+$projectName = Read-Host -Prompt "[ex: rockerbros]" 
+Write-Host ""
+Write-Host ""
+Write-Host "-----------------------------------------------" -ForeGround Cyan
+Write-Host "Azure resource provisioning started [$projectName]" -ForeGround Cyan
+Write-Host "-----------------------------------------------" -ForeGround Cyan
+Write-Host ""
+Write-Host ""
+
 New-AzResourceGroupDeployment `
     -Name WordPressDeploy `
     -ResourceGroupName WP1 `
     -TemplateFile .\template.json `
     -TemplateParameterFile .\parameters.json `
+    -resource_name $projectName `
     -Verbose
+
+Write-Host ""
+Write-Host ""
+Write-Host ""
+
+Write-Host "Completed View site https://$projectName.azurewebsites.net" -ForeGround Green
