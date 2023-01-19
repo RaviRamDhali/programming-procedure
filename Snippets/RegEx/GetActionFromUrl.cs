771eeb434b192773b05d0f0d08b64e06230559cf
Ravi Ram <ravi@dhali.com>
2023-01-19 09:43:39 -0800

    Update GetActionFromUrl.cs

--- Snippets/RegEx/GetActionFromUrl.cs
+++ Snippets/RegEx/GetActionFromUrl.cs
@@ -1,14 +1,15 @@
 private static string GetActionFromUrl(string value)
-        {
-            string pattern = @"action=([^&]*)";
+{
+        var str = "/pages/new?page=danny&action=nick&ref=ravi";
+        string pattern = @"action=([^&]*)";
 
-            Regex rg = new Regex(pattern, RegexOptions.IgnoreCase);
-            Match s = rg.Match(value);
-            
-            if (s.IsNull() || s.Groups[1].IsNull())
-                return string.Empty;
-            
-            string result = s.Groups[1].Value.ToStringOrDefault().ToLower();
+        Regex rg = new Regex(pattern, RegexOptions.IgnoreCase);
+        Match s = rg.Match(value);
 
-            return result;
-        }
+        if (s.IsNull() || s.Groups[1].IsNull())
+        return string.Empty;
+
+        string result = s.Groups[1].Value.ToStringOrDefault().ToLower();
+
+        return result;
+}
